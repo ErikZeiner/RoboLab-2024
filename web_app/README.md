3b5e6782e2b495a650dc8dcbc301ed751c529dde
Erik Zeiner <47943924+ErikZeiner@users.noreply.github.com>
2024-07-08 23:36:34 +0200

    fully move to js

--- web_app/README.md
+++ web_app/README.md
@@ -1,8 +1,30 @@
 # TODO
 
+- [ ] menu with more settings
+- [ ] regex for matching users input
+- [ ] pygame visualisation
+- [ ] table for checking validity
+- [ ] user usable methods doing visualisation and validity
+- [ ] 
+
+run input against a python regex, if it passes that 
+
+check
+and visualisation on two layers
+one simulates cursor movements allowing me to use pygame
+one that check against a table representation of the maze
+
+- map backend
+- map visualisation
+- regex to match users code and 
+- more ui to look more done, options and such
+- read prompt from file
+
+
 # Notes
 - text editor - https://github.com/WebCoder49/code-input
 - https://ai.google.dev/gemini-api/docs/quickstart?lang=python
 Because standard does not work in Germany
 https://cloud.google.com/vertex-ai#build-with-gemini
 https://www.30secondsofcode.org/js/s/word-wrap/
+https://www.w3schools.com/howto/howto_css_dropdown.asp
--- web_app/script.js
+++ web_app/script.js
@@ -1,27 +1,52 @@
 import {GoogleGenerativeAI} from "@google/generative-ai";
-const wordWrap = (str, max, br = '\n') => str.replace(
-  new RegExp(`(?![^\\n]{1,${max}}$)([^\\n]{1,${max}})\\s`, 'g'), `$1${br}`
-);
+
     // Fetch your API_KEY
     const API_KEY = "AIzaSyB1eqX5raYM4CmkVP044nkU6Dp9KqDIALM";
-    // Reminder: This should only be for local testing
 
     // Access your API key (see "Set up your API key" above)
     const genAI = new GoogleGenerativeAI(API_KEY);
-
-    // ...
 
     // The Gemini 1.5 models are versatile and work with most use cases
     const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"});
 
     async function sendRequest(prompt) {
-        const result = await model.generateContent(prompt);
-        const response = await result.response;
-        const text = response.text();
-        return text;
+        try {
+            console.log("prompt" + prompt);
+            const result = await model.generateContent(prompt);
+            const response = await result.response;
+            console.log("response" + response.text());
+            const text = response.text();
+            return text;
+        }
+        catch(e){
+            document.getElementById('aiHelperWindow').value = "Sorry, I can't help you with that.";
+        }
+    }
+
+        function testCode(text){
+        console.log(text);
+        var regex = /(move(Right|Left|Up|Down|)\(([0-9]{1,2})\)\n?)+/;
+        return regex.test(text);
+
     }
 
     document.getElementById('userQueryBtn').onclick = async () => {
-        const prompt = document.getElementById('userQuery').value;
-        document.getElementById('aiHelperWindow').innerHTML = await sendRequest(prompt);
+        document.getElementById('aiHelperWindow').value = "Hmm, let me think...";
+        document.getElementById('aiHelperWindow').value = await sendRequest(document.getElementById('userQuery').value);
     }
+    document.getElementById('runBtn').onclick = async () => {
+        if (testCode(document.getElementById('userCode').value)){
+            console.log("regex ok");
+        } else {
+            document.getElementById('aiHelperWindow').value = "Your code is not correct.";
+            document.getElementById('aiHelperWindow').value = await sendRequest("help");
+        }
+    }
+
+
+
+
+
+    window.addEventListener('load', function() {
+        document.getElementById('aiHelperWindow').value = "Hi, name is ... and I am here to help you!";
+})
