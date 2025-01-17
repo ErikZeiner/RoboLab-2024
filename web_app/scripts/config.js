4e757b97f8a114a554f3eaf040ebba5b960273f4
Erik Zeiner <47943924+ErikZeiner@users.noreply.github.com>
2025-01-17 17:09:26 +0100

    add note to config.js

--- web_app/scripts/config.js
+++ web_app/scripts/config.js
@@ -1,4 +1,5 @@
 // ENTER YOUR GEMINI API KEY HERE - IT IS NOT POSSIBLE TO USE GEMINI API FROM WITHIN THE EU WITHOUT A VPN
+// This part of the app needs to be improved security-wise. Due to lack of more time for a proper implementation, a temporary API key, which is no longer functional, was used.
 export let config = {
   KEY: '',
 }
